6f2efa7714a359d4a989d0e067a464b1d261f431
Tony WANG <tony.wang.001@student.uni.lu>
2024-11-16 14:20:55 +0100

    error

--- app/src/main/java/test/App.java/App.java
+++ app/src/main/java/test/App.java/App.java
@@ -3,6 +3,7 @@ package main.java.test.App.java;
 
 public class App{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World")
+        System.out.println("This shouldnt work");
     }
 }
