80d24d558438dfbb5038b4e1d7a4622d3cf267c4
Tony WANG <tony.wang.001@student.uni.lu>
2024-11-16 15:19:47 +0100

    error again

--- app/src/main/java/test/App.java/App.java
+++ app/src/main/java/test/App.java/App.java
@@ -4,6 +4,6 @@ package main.java.test.App.java;
 public class App{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("This shouldnt work");
+        System.out.println("This shouldnt work")
     }
 }
