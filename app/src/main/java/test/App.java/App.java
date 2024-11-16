9e31d49a1543c2404506d1977f10d0596044b0b3
Tony WANG <tony.wang.001@student.uni.lu>
2024-11-16 14:30:42 +0100

    error

--- app/src/main/java/test/App.java/App.java
+++ app/src/main/java/test/App.java/App.java
@@ -5,5 +5,6 @@ public class App{
     public static void main(String[] args){
         System.out.println("Hello World")
         System.out.println("This shouldnt work");
+        why is this builidn
     }
 }
