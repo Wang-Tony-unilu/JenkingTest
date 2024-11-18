b63ea55001b3f707907700de34c0979df54ce456
Tony WANG <tony.wang.001@student.uni.lu>
2024-11-18 15:08:45 +0100

    Trigger Build 2

--- app/src/main/java/test/App.java/App.java
+++ app/src/main/java/test/App.java/App.java
@@ -4,7 +4,7 @@ package main.java.test.App.java;
 public class App{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Trigger build");
+        System.out.println("Trigger build again");
         
     }
 }
